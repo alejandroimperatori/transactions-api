eac2f6d33b62647a30f16b4cf726ee16e86fad7c
Ale <alejandro.imperatori@modak.live>
2026-04-08 18:39:17 -0300

    set up spring boot app with docker

---
+++ src/test/java/com/transactions/api/TransactionsApiApplicationTest.java
@@ -0,0 +1,12 @@
+package com.transactions.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
